f0ccf0c4c19bb017300e885713c9d7d53d01ab8c
Tanya Ly <tanya.lytovchenko@gmail.com>
2019-01-26 14:33:06 +0100

    logic game js file

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// js
+var level = 1;
+var operation = "";
+
+// first and second numbers for game
+var firstNumber = 0;
+var secondNumber = 0;
+
+// Make list with 3 answers (2 wrong, 1 right)
+var answer = [];
+
+function prepareGame() {
+
+    operation = "+";
+    // returns a random integer from 1 to 5
+    firstNumber = Math.floor(Math.random() * 5) + 1;
+    secondNumber = Math.floor(Math.random() * 5) + 1;
+
+    // add wrong answers until we have 2
+    while (answer.length < 2) {
+        var variant = Math.floor(Math.random() * 10) + 1;
+        if (variant == firstNumber + secondNumber) {
+            continue;
+        }
+        answer.push(variant);
+    }
+
+    // add right answer
+    answer.push(firstNumber + secondNumber);
+
+    // shuffle 
+    answer.sort(function (a, b) {
+        return Math.floor(Math.random() * 2);
+    });
+
+}
+
+prepareGame();
